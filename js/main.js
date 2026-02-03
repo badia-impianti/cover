9fae7f3e4b396ce1dc4bd03ff72f0b405427d961
mattiarainieri <mattiarainieri@live.it>
2026-02-03 14:14:20 +0100

    added contact card for filippo

--- js/main.js
+++ js/main.js
@@ -39,3 +39,34 @@ document.addEventListener("DOMContentLoaded", function () {
   revealOnScroll();
 });
 
+
+ function downloadVCard() {
+  // Dati di Filippo Badia
+  const contact = {
+    firstName: "Filippo",
+    lastName: "Badia",
+    phone: "+393478380328", // Inserisci il numero reale
+    email: "filippo@badiasilvano.it", // Inserisci l'email reale
+    org: "Badia Silvano di Badia Andrea"
+  };
+
+  // Costruzione vCard standard
+  const vcard = "BEGIN:VCARD\n" +
+                "VERSION:3.0\n" +
+                "FN:" + contact.firstName + " " + contact.lastName + "\n" +
+                "N:" + contact.lastName + ";" + contact.firstName + ";;;\n" +
+                "TEL;TYPE=CELL:" + contact.phone + "\n" +
+                "EMAIL:" + contact.email + "\n" +
+                "ORG:" + contact.org + "\n" +
+                "END:VCARD";
+
+  const blob = new Blob([vcard], { type: "text/vcard" });
+  const url = window.URL.createObjectURL(blob);
+  
+  const newLink = document.createElement('a');
+  newLink.download = contact.firstName + "_" + contact.lastName + ".vcf";
+  newLink.href = url;
+  
+  newLink.click();
+  window.URL.revokeObjectURL(url);
+}
